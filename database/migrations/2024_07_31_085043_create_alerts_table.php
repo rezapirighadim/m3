a8c01502cb351b125a672dcffe38edd1b352aeb8
rezapirighadim <reza.piryghadim@gmail.com>
2024-07-31 12:33:09 +0330

    add m3 models and db's

---
+++ database/migrations/2024_07_31_085043_create_alerts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sensor_id');
+            $table->string('sensor_uuid');
+            $table->json('alert_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
